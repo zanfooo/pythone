4f5d18eff33f4343877713a46fceadbfd2c36cf6
zanfooo <152177598+zanfooo@users.noreply.github.com>
2026-05-21 17:07:32 +0200

    Update client.py

--- palestra/client.py
+++ palestra/client.py
@@ -1,17 +1,20 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 5000))
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 5000))
 
-abbonamento = input("Inserisci abbonamento (mensile/annuale): ")
-eta = input("Inserisci età: ")
-corsi = input("Inserisci numero corsi extra: ")
+    abbonamento = input("Inserisci abbonamento (mensile/annuale): ")
+    eta = input("Inserisci età: ")
+    corsi = input("Inserisci numero corsi extra: ")
 
-client.send(abbonamento.encode())
-client.send(eta.encode())
-client.send(corsi.encode())
+    client.sendall(abbonamento.encode())
+    client.sendall(eta.encode())
+    client.sendall(corsi.encode())
 
-risposta = client.recv(1024).decode()
-print(risposta)
+    risposta = client.recv(1024).decode()
+    print("Risposta dal server:", risposta)
+    client.close()
 
-client.close()
+if __name__ == "__main__":
+    start_client()
